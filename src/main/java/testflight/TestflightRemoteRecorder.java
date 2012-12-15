f0d4d20b8d051c68ae5b4e2528227637b0f9d325
Michael Koch <michael.koch@enough.de>
2012-12-15 13:21:58 +0100

    If no dsym given in settings search for it

--- src/main/java/testflight/TestflightRemoteRecorder.java
+++ src/main/java/testflight/TestflightRemoteRecorder.java
@@ -2,7 +2,6 @@ package testflight;
 
 import hudson.model.BuildListener;
 import hudson.remoting.Callable;
-import org.apache.commons.lang.StringUtils;
 
 import java.io.File;
 import java.io.Serializable;
@@ -14,20 +13,20 @@ import java.util.List;
  */
 public class TestflightRemoteRecorder implements Callable<Object, Throwable>, Serializable {
     final private boolean pathSpecified;
+    final private boolean dsymPathSpecified;
     final private TestflightUploader.UploadRequest uploadRequest;
     final private BuildListener listener;
 
-    public TestflightRemoteRecorder(boolean pathSpecified, TestflightUploader.UploadRequest uploadRequest, BuildListener listener) {
+    public TestflightRemoteRecorder(boolean pathSpecified, boolean dsymPathSpecified, TestflightUploader.UploadRequest uploadRequest, BuildListener listener) {
         this.pathSpecified = pathSpecified;
+        this.dsymPathSpecified = dsymPathSpecified;
         this.uploadRequest = uploadRequest;
         this.listener = listener;
     }
 
     public Object call() throws Throwable {
-        if (!StringUtils.isEmpty(uploadRequest.dsymPath))
-            uploadRequest.dsymFile = new File(uploadRequest.dsymPath);
-
         uploadRequest.file = identifyIpa();
+        uploadRequest.dsymFile = identifyDsym();
 
         listener.getLogger().println(uploadRequest.file);
 
@@ -54,6 +53,32 @@ public class TestflightRemoteRecorder implements Callable<Object, Throwable>, Se
                     return ipaResult;
             }
             else if (file.getName().endsWith(".ipa"))
+            {
+                return file;
+            }
+        }
+        return null;
+    }
+
+    private File identifyDsym() {
+        if (dsymPathSpecified) {
+            return new File(uploadRequest.dsymPath);
+        } else {
+            File workspaceDir = new File(uploadRequest.dsymPath);
+            File possibleDsym = TestflightRemoteRecorder.findDsym(workspaceDir);
+            return possibleDsym != null ? possibleDsym : workspaceDir;
+        }
+    }
+
+    public static File findDsym(File root) {
+        for (File file : root.listFiles()) {
+            if (file.isDirectory())
+            {
+                File dsymResult = findDsym(file);
+                if(dsymResult != null)
+                    return dsymResult;
+            }
+            else if (file.getName().endsWith("-dSYM.zip"))
             {
                 return file;
             }
