eca046918e8bd789f82f6cf398bdf73fb13c8ec2
Jerome Lacoste <jerome.lacoste@gmail.com>
2013-03-13 12:34:15 +0100

    Support APKs

--- src/main/java/testflight/TestflightRemoteRecorder.java
+++ src/main/java/testflight/TestflightRemoteRecorder.java
@@ -18,7 +18,7 @@ import org.apache.commons.io.filefilter.TrueFileFilter;
 /**
  * Code for sending a build to TestFlight which can run on a master or slave.
  * 
- * When the ipa file or optional dsym file are not specified, this class first tries to resolve their paths, searching them inside the workspace.
+ * When the ipa/apk file or optional dsym file are not specified, this class first tries to resolve their paths, searching them inside the workspace.
  */
 public class TestflightRemoteRecorder implements Callable<Object, Throwable>, Serializable {
     final private String remoteWorkspace;
@@ -35,17 +35,21 @@ public class TestflightRemoteRecorder implements Callable<Object, Throwable>, Se
         HashMap result = new HashMap();
         TestflightUploader uploader = new TestflightUploader();
 
-        Collection<File> ipaFiles = findIpaFiles(uploadRequest.filePath);
-        for (File ipaFile : ipaFiles) {
-            uploadRequest.file = ipaFile;
-            uploadRequest.dsymFile = identifyDsym(uploadRequest.dsymPath, ipaFile.getName());
+        Collection<File> ipaOrApkFiles = findIpaOrApkFiles(uploadRequest.filePath);
+        for (File ipaOrApkFile : ipaOrApkFiles) {
+            boolean isIpa = ipaOrApkFile.getName().endsWith(".ipa");
+            uploadRequest.file = ipaOrApkFile;
+            if (isIpa) {
+                uploadRequest.dsymFile = identifyDsym(uploadRequest.dsymPath, ipaOrApkFile.getName());
+            }
 
-            listener.getLogger().println("IPA: " + uploadRequest.file);
-            listener.getLogger().println("DSYM: " + uploadRequest.dsymFile);
+            listener.getLogger().println("File: " + uploadRequest.file);
+            if (isIpa) {
+                listener.getLogger().println("DSYM: " + uploadRequest.dsymFile);
+            }
 
             long startTime = System.currentTimeMillis();
             result.putAll(uploader.upload(uploadRequest));
-
             long time = System.currentTimeMillis() - startTime;
 
             float speed = computeSpeed(time);
@@ -99,13 +103,13 @@ public class TestflightRemoteRecorder implements Callable<Object, Throwable>, Se
         return dsymFile;
     }
 
-    /* if a specified filePath is specified, return it, otherwise find recursively all ipa files in the remoteworkspace */
-    private Collection<File> findIpaFiles(String filePath) {
+    /* if a specified filePath is specified, return it, otherwise find recursively all ipa/apk files in the remoteworkspace */
+    private Collection<File> findIpaOrApkFiles(String filePath) {
         Collection<File> files;
         if (filePath != null && !filePath.trim().isEmpty()) {
             files = Collections.singleton(new File(filePath));
         } else {
-            String[] extensions = {"ipa"};
+            String[] extensions = {"ipa", "apk"};
             boolean recursive = true;
             files = FileUtils.listFiles(new File(remoteWorkspace), extensions, recursive);
         }
